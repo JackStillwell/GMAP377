06f6cc0b84de8527a57481a4c9721f168df50bf6
ztc <tzcliff@outlook.com>
2018-04-17 14:54:16 -0400

    Enemy AI

---
+++ Shade/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+	[SerializeField]
+	private GameObject Player;
+
+
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.color = Player.GetComponent<Renderer>().material.GetColor("_Color");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
