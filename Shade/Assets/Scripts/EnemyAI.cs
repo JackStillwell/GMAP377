6501d65dbfda65490f0aeac3294e00b7acb0aa3c
ztc <tzcliff@outlook.com>
2018-04-17 15:17:03 -0400

    Enemy AI
    Enemy detects Player's color changing.
    
    Currently Player changes its color from blue to red then to green, every 3 seconds.
    
    In Enemy AI script, the enemy could fetch the Player's color info.

--- Shade/Assets/Scripts/EnemyAI.cs
+++ Shade/Assets/Scripts/EnemyAI.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour {
 	[SerializeField]
 	private GameObject Player;
+	private Color PlayerColor;
 
 
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<Renderer>().material.color = Player.GetComponent<Renderer>().material.GetColor("_Color");
+		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
+		StartCoroutine ("waitThreeSeconds");
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+
+
 	}
+
+	IEnumerator waitThreeSeconds(){
+
+		Player.GetComponent<Renderer> ().material.color = Color.blue;
+		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
+		Debug.Log (PlayerColor);
+			yield return new WaitForSeconds (3);
+		Player.GetComponent<Renderer> ().material.color = Color.red;
+		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
+		Debug.Log (PlayerColor);
+			yield return new WaitForSeconds (3);
+		Player.GetComponent<Renderer> ().material.color = Color.green;
+		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
+		Debug.Log (PlayerColor);
+	}
+
+
+
 }
