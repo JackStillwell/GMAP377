802efbeaa36b336d3d43f456b0a93afbbca2c2c2
DESKTOP-CSBCMKL\Erin <nern.strudel@gmail.com>
2018-04-17 08:28:46 -0400

    started camera and player-end color change scripts

---
+++ Shade/Assets/Scripts/Camera/ColorRaycaster.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRaycaster : MonoBehaviour {
+
+
+    public GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    bool colorCheck()
+    {
+        Ray r = new Ray(gameObject.transform.position, (playerObject.transform.position - gameObject.transform.position));
+
+        return false;
+
+    }
+}
