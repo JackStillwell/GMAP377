23a91c3348641d0b7b5e6c3693b6aa0e37e5a1d9
Tiancheng Zhu <tz85@drexel.edu>
2018-04-17 16:55:46 -0400

    Enemy AI
    What it does:
    
    The script will change player’s color every 3 seconds(Yellow >Blue>Green -> enemy’s Color )
    
    Check player’s color every frame.
    
    Currently it prints out log message of player’s color
    
    How to use it:
    
    Attach Enemy AI scripts on Enemy prefab

--- Shade/Assets/Scripts/Enemy/EnemyAI.cs
+++ Shade/Assets/Scripts/Enemy/EnemyAI.cs
@@ -6,36 +6,69 @@ public class EnemyAI : MonoBehaviour {
 	[SerializeField]
 	private GameObject Player;
 	private Color PlayerColor;
+	private Color EnemyColor;
 
 
 
 	// Use this for initialization
 	void Start () {
+
+		EnemyColor = GetComponent<Renderer>().material.GetColor("_Color");
+		Debug.Log (EnemyColor);
 		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
 		StartCoroutine ("waitThreeSeconds");
+
+
+
 	}
 
 	// Update is called once per frame
 	void Update () {
 
+		EnemyColor = GetComponent<Renderer>().material.GetColor("_Color");
+		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
 
+		//Debug.Log (IsEqualTo(PlayerColor, EnemyColor));
+		if (IsEqualTo(PlayerColor, EnemyColor)) {
+			Debug.Log ("Same Color right here");
+		}
 
 	}
 
-	IEnumerator waitThreeSeconds(){
+	public  bool IsEqualTo(Color me, Color other)
+	{
+		return me.r == other.r&& me.g == other.g && me.b == other.b && me.a == other.a;
+	}
 
-		Player.GetComponent<Renderer> ().material.color = Color.blue;
+	IEnumerator waitThreeSeconds(){
+		Player.GetComponent<Renderer> ().material.color = Color.yellow;
 		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
 		Debug.Log (PlayerColor);
 			yield return new WaitForSeconds (3);
-		Player.GetComponent<Renderer> ().material.color = Color.red;
+		Player.GetComponent<Renderer> ().material.color = Color.blue;
 		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
 		Debug.Log (PlayerColor);
 			yield return new WaitForSeconds (3);
 		Player.GetComponent<Renderer> ().material.color = Color.green;
 		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
 		Debug.Log (PlayerColor);
+			yield return new WaitForSeconds (3);
+		Player.GetComponent<Renderer> ().material.color = EnemyColor;
+		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
+		Debug.Log (PlayerColor);
+
+	
+
+
+
 	}
+
+
+
+
+
+
+		
 
 
 
