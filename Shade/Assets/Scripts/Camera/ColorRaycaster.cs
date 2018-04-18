cd59101199f120f0300f4ec272bfc4214a74a772
DESKTOP-CSBCMKL\Erin <nern.strudel@gmail.com>
2018-04-17 23:47:58 -0400

    color vision is now collider-based for optimization purposes. collider trigger added to enemy prefab

--- Shade/Assets/Scripts/Camera/ColorRaycaster.cs
+++ Shade/Assets/Scripts/Camera/ColorRaycaster.cs
@@ -10,51 +10,93 @@ public class ColorRaycaster : MonoBehaviour {
 
     public Color playerView;
 
+    private List<GameObject> currentlyColliding;
+
 	// Use this for initialization
 	void Start () {
 
         //playerMat = playerObject.GetComponent<Material>();
+        currentlyColliding = new List<GameObject>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        colorCheck();
 	}
 
-    bool colorCheck()
+    /// <summary>
+    /// Adds the new object to the currentlyColliding list
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
     {
-        Ray rayDirection = new Ray(gameObject.transform.position, (target.transform.position - gameObject.transform.position));
+        currentlyColliding.Add(other.gameObject);
+        colorCheck();
+    }
 
-        Physics.Raycast(rayDirection, out hitInfo, (target.transform.position - gameObject.transform.position).magnitude);
+    /// <summary>
+    /// Removes exiting object from the currentlyColliding list.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        currentlyColliding.Remove(other.gameObject);
+        colorCheck();
+    }
 
-        if(hitInfo.transform.gameObject != target)
+    public bool colorCheck()
+    {
+
+        if (currentlyColliding.Count > 1)
         {
+            bool foundTarget = false;
 
-            //get the color of both objects and then math needs to happen
-            Color playerColor = target.realColor;
-            GameObject hitobj = hitInfo.transform.gameObject;
-            Color objColor = hitobj.GetComponent<MeshRenderer>().material.color;
+            foreach(GameObject g in currentlyColliding)
+            {                
+                if(g.name == target.name)
+                {
+                    foundTarget = true;
+                    break;
+                }       
+            }
 
-            Color newColor = Color.white;
 
-            newColor.a = playerColor.a;
+            if (foundTarget)
+            {
 
-            newColor.r = ((playerColor.r + (objColor.r * objColor.a)) / 2);
-            newColor.g = ((playerColor.g + (objColor.g * objColor.a)) / 2);
-            newColor.b = ((playerColor.b + (objColor.b * objColor.a)) / 2);
+                //now see if player is behind or in front of object
+                Physics.Raycast(gameObject.transform.position, (target.transform.position - gameObject.transform.position), out hitInfo);
+                if (hitInfo.transform.gameObject.name != target.name)
+                {
+                    
+                    //get the color of both objects and then math needs to happen
+                    Color playerColor = target.realColor;
+                    // GameObject hitobj = hitInfo.transform.gameObject;
+                    Color objColor = hitInfo.transform.gameObject.GetComponent<MeshRenderer>().material.color;
 
-            playerView = newColor;
+                    Color newColor = Color.white;
 
-            Debug.Log(playerView.ToString());
+                    newColor.a = playerColor.a;
 
-            return true;
+                    newColor.r = ((playerColor.r + (objColor.r * objColor.a)) / 2);
+                    newColor.g = ((playerColor.g + (objColor.g * objColor.a)) / 2);
+                    newColor.b = ((playerColor.b + (objColor.b * objColor.a)) / 2);
+
+                    Debug.Log("YES");
+
+                    playerView = newColor;
+
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+            
         }
         else
         {
-            Debug.Log(playerView.ToString());
             return false;
         }
-        
+
 
     }
 }
