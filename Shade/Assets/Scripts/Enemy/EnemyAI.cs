ae51706b63880820a9f101da2c5adf9b68c418df
ztc <tzcliff@outlook.com>
2018-04-18 00:08:11 -0400

    AI update

--- Shade/Assets/Scripts/Enemy/EnemyAI.cs
+++ Shade/Assets/Scripts/Enemy/EnemyAI.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour {
 	[SerializeField]
 	private GameObject Player;
+	[SerializeField]
+	private GameObject Camera;
 	private Color PlayerColor;
 	private Color EnemyColor;
 
@@ -16,6 +18,7 @@ public class EnemyAI : MonoBehaviour {
 		EnemyColor = GetComponent<Renderer>().material.GetColor("_Color");
 		Debug.Log (EnemyColor);
 		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
+		//ColorRaycaster colorRaycaster = 
 		StartCoroutine ("waitThreeSeconds");
 
 
@@ -28,7 +31,11 @@ public class EnemyAI : MonoBehaviour {
 		EnemyColor = GetComponent<Renderer>().material.GetColor("_Color");
 		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
 
-		//Debug.Log (IsEqualTo(PlayerColor, EnemyColor));
+
+
+
+
+
 		if (IsEqualTo(PlayerColor, EnemyColor)) {
 			Debug.Log ("Same Color right here");
 		}
@@ -39,7 +46,7 @@ public class EnemyAI : MonoBehaviour {
 	{
 		return me.r == other.r&& me.g == other.g && me.b == other.b && me.a == other.a;
 	}
-
+	// change Player's color
 	IEnumerator waitThreeSeconds(){
 		Player.GetComponent<Renderer> ().material.color = Color.yellow;
 		PlayerColor = Player.GetComponent<Renderer>().material.GetColor("_Color");
