8697790904fbfe5fd6b35e78faba3288e4c6df73
Justin <jseara2011@gmail.com>
2018-04-21 19:09:13 -0400

    Colored Lights Version B
    Added Colored Lights prefab
    Changed ground color
    Changed Enemy material
    Added color lights script.  Once it enters the collider, change color of player while inside the area.

--- Shade/Assets/Scripts/Objects/ColoredLight.cs
+++ Shade/Assets/Scripts/Objects/ColoredLight.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColoredLight : MonoBehaviour {
+public class ColoredLight : MonoBehaviour
+{
+	Color originalColor;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        originalColor = other.gameObject.GetComponent<Renderer>().material.color;
+        Color lightColor = transform.GetComponent<Light>().color;
+        Color newColor = Color.white;
+
+        newColor.r = ((originalColor.r + (lightColor.r * lightColor.a)) / 2);
+        newColor.g = ((originalColor.g + (lightColor.g * lightColor.a)) / 2);
+        newColor.b = ((originalColor.b + (lightColor.b * lightColor.a)) / 2);
+
+		other.gameObject.GetComponent<Renderer>().material.color = newColor;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+		other.gameObject.GetComponent<Renderer>().material.color = originalColor;
+    }
 }
