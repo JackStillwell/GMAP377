efdfd382406698074979b7761326604c382126fd
Jack Stillwell <jack.stillwell@outlook.com>
2018-04-25 15:38:12 -0400

    everything is now working!

--- Shade/Assets/Scripts/Player/PlayerController.cs
+++ Shade/Assets/Scripts/Player/PlayerController.cs
@@ -4,22 +4,31 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 3.0f;
 
+    private float _x;
+    private float _y;
+    private float _z;
+
     private Rigidbody _pcRigidbody;
 
     void Start()
     {
         _pcRigidbody = GetComponent<Rigidbody>();
+        // Freezes X and Z rotation so the player cannot fall over and can only rotate side to side.
+        _pcRigidbody.constraints = (RigidbodyConstraints) 80;
     }
-    
+
     void Update()
     {
-        var y = Input.GetAxis("Mouse X");
-        
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * _moveSpeed;
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * _moveSpeed;
-        
-        Vector3 moveVector = _pcRigidbody.position + _pcRigidbody.rotation * new Vector3(x, 0, z);
-        Quaternion moveQuaternion = Quaternion.Euler(new Vector3(0, y, 0));
+        _y = Input.GetAxis("Mouse X");
+
+        _z = Input.GetAxis("Vertical") * Time.deltaTime * _moveSpeed;
+        _x = Input.GetAxis("Horizontal") * Time.deltaTime * _moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveVector = _pcRigidbody.position + _pcRigidbody.rotation * new Vector3(_x, 0, _z);
+        Quaternion moveQuaternion = Quaternion.Euler(new Vector3(0, _y, 0));
         
         _pcRigidbody.MoveRotation(transform.rotation * moveQuaternion);
         _pcRigidbody.MovePosition(moveVector);
