835975fae13322f4c46c00e33e782dadd3ffa6ed
Jack Stillwell <jack.stillwell@outlook.com>
2018-04-29 01:16:46 -0400

    initial version of the ai, provides a method which returns true when enemy is in the collider

---
+++ Shade/Assets/Scripts/Enemy/StaticEnemyAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticEnemyAI : MonoBehaviour
+{
+	private Collider _sight;
+	private bool _playerIsVisible;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_sight = GetComponent<Collider>();
+	}
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.name == "Player")
+			_playerIsVisible = true;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.name == "Player")
+			_playerIsVisible = false;
+	}
+
+	public bool IsPlayerVisible()
+	{
+		return _playerIsVisible;
+	}
+}
