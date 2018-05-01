16ce914a2d5e965ca9036487218d4eb97d6d5b0e
Justin <jseara2011@gmail.com>
2018-04-30 22:44:26 -0400

    Finished skeleton of Pause Menu

---
+++ Shade/Assets/Pause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pause_screen;
+    // Use this for initialization
+    void Start()
+    {
+        pause_screen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Toggle();
+
+    }
+
+    void Toggle()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            pause_screen.SetActive(false);
+        }
+        else if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            pause_screen.SetActive(true);
+        }
+    }
+}
