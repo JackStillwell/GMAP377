83a017825556d58414562d502403d96d03dad666
Jack Stillwell <jack.stillwell@outlook.com>
2018-05-08 16:08:43 -0400

    updated EnemySight

--- Shade/Assets/Scripts/Enemy/EnemySight.cs
+++ Shade/Assets/Scripts/Enemy/EnemySight.cs
@@ -1,151 +1,112 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.AI;
 
 public class EnemySight : MonoBehaviour
 {
-	// Is true only if Player is in FoV and wrong color
-	private bool _playerVisible;
+    // Is true only if Player is in FoV and wrong color
+    private bool _playerVisible;
 
-	private GameObject _player;
-	private MeshCollider _col;
+    private GameObject _player;
+    private MeshCollider _col;
 
-	private Color _percievedPlayerColor;
-	private NavMeshAgent _nav;
-	private EnemyPatrolAI _patrolAi;
+    private Color _percievedPlayerColor;
+    private NavMeshAgent _nav;
+    private EnemyPatrolAI _patrolAi;
 
-	private RaycastHit[] _hitArray;
+    private RaycastHit[] _hitArray;
 
-	void Start()
-	{
-		_patrolAi = GetComponent<EnemyPatrolAI> ();
-		_nav = GetComponent<NavMeshAgent> ();
-		_col = GetComponent<MeshCollider> ();
-		_player = GameObject.FindGameObjectWithTag("Player");
-
-		_percievedPlayerColor = GetPlayerColor(_player);
-		_player = GameObject.FindGameObjectWithTag("Player");
-		_playerVisible = false;
-	}
-	
-	private void OnTriggerEnter(Collider other)
+    void Start()
     {
-	    if (other.CompareTag("Player"))
-	    {
-		    _percievedPlayerColor = GetPlayerColor(_player);
-		    VisibleCheck();
-	    }
+        _patrolAi = GetComponent<EnemyPatrolAI> ();
+        _nav = GetComponent<NavMeshAgent> ();
+        _col = GetComponent<MeshCollider> ();
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+        _percievedPlayerColor = GetPlayerColor(_player);
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _playerVisible = false;
+    }
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _percievedPlayerColor = GetPlayerColor(_player);
+            VisibleCheck();
+        }
     }
 
-	private void OnTriggerStay(Collider other)
-	{
-		if (other.tag == "Player")
-		{
-			_percievedPlayerColor = GetPlayerColor(_player);
-		}
-		/*
-		Debug.Log (_playerVisible);
-		//Debug.Log (other.gameObject);
-		//get the direction of the object
-	    Vector3 direction = other.transform.position - transform.position;
-	    
-	    // get the angle between forward and the object
-	    float angle = Vector3.Angle(direction, transform.forward);
-		// note: Playervua
-		if (angle < _fieldOfViewAngle * 0.5f && !_allObjectsInSight.Contains(other.gameObject) && ChangesColor(other.tag))
-			_allObjectsInSight.Add(other.gameObject);
-		
-		
-	    // remove objects in collider but not in sight
-	    if (!(angle < _fieldOfViewAngle * 0.5f) && _allObjectsInSight.Contains(other.gameObject)) 
-		    _allObjectsInSight.Remove(other.gameObject);
-
-		// after all that, see if player is in the list and check visibility
-		if (_allObjectsInSight.Contains (_player)) {
-			Debug.Log("visible check 2");
-			VisibleCheck();
-		} */
-	}
-
-	void OnTriggerExit(Collider other)
-	{
-		/*if (other.gameObject == _player)
-		{
-			_allObjectsInSight.Remove(other.gameObject);
-			_playerVisible = false;
-			_patrolAi.NextPoint ();
-		}*/
-
-		if (other.tag == "Player")
-	    {
-		    _playerVisible = false;
-	    }
-	}
-
-	private void VisibleCheck()
-	{
-		var direction = _player.transform.position - transform.position;
-		_hitArray = Physics.RaycastAll(transform.position, direction.normalized, direction.magnitude);
-
-		foreach (var hit in _hitArray)
-		{	
-			if (hit.collider.gameObject == _player)
-			{
-				ColorPercievedUpdate();
-				_playerVisible = true;
-				break;
-			}
-		}
-	}
-
-	private void ColorPercievedUpdate()
+    private void OnTriggerStay(Collider other)
     {
-        if (_allObjectsInSight.Count > 1)
+        if (other.tag == "Player")
         {
-			Debug.Log ("Color with glass");
-            //now see if player is behind or in front of object
-			RaycastHit[] _hitArray1;
-            _hitArray1 = Physics.RaycastAll(gameObject.transform.position, _player.transform.position - gameObject.transform.position);
+            _percievedPlayerColor = GetPlayerColor(_player);
+        }	
+    }
 
-	        if (_hitArray1.Length > 1)
-	        {
-				foreach (var hitInfo in _hitArray1)
-		        {
-			        if (hitInfo.transform.gameObject != _player)
-			        {
-				        // GameObject hitobj = hitInfo.transform.gameObject;
-//				        Color objColor = hitInfo.transform.gameObject.GetComponent<MeshRenderer>().material.color;
-//
-//				        Color newColor = Color.white;
-//
-//				        newColor.a = _percievedPlayerColor.a;
-//
-//				        newColor.r = ((_percievedPlayerColor.r + (objColor.r * objColor.a)) / 2);
-//				        newColor.g = ((_percievedPlayerColor.g + (objColor.g * objColor.a)) / 2);
-//				        newColor.b = ((_percievedPlayerColor.b + (objColor.b * objColor.a)) / 2);
-//
-//				        _percievedPlayerColor = newColor;
-						_percievedPlayerColor = _player.GetComponentInChildren<Renderer>().material.color;
-						Debug.Log (_percievedPlayerColor);
-			        }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _playerVisible = false;
+        }
+    }
 
-			        else
-			        {
-				        break;
-			        }
-		        }
-	        }
+    private void VisibleCheck()
+    {
+        var direction = _player.transform.position - transform.position;
+        _hitArray = Physics.RaycastAll(transform.position, direction.normalized, direction.magnitude);
 
-	        else
-	        {
-		        _percievedPlayerColor = _player.GetComponentInChildren<Renderer>().material.color;
-	        }
+        foreach (var hit in _hitArray)
+        {
+            if (hit.collider.gameObject == _player)
+            {
+                ColorPercievedUpdate();
+                _playerVisible = true;
+                break;
+            }
+
+            if (ChangesColor(hit.collider.tag)) ;  // Do Nothing and Continue
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    private void ColorPercievedUpdate()
+    {
+        if (_hitArray.Length > 1)
+        {
+            foreach (var hitInfo in _hitArray)
+            {
+                if (hitInfo.transform.gameObject != _player)
+                {
+                    Color objColor = GetColorValue((ColorName) Enum.Parse(typeof(ColorName), hitInfo.collider.tag));
+
+                    Color newColor = new Color();
+
+                    newColor.a = _percievedPlayerColor.a;
+
+                    newColor.r = (_percievedPlayerColor.r + objColor.r) / 2;
+                    newColor.g = (_percievedPlayerColor.g + objColor.g) / 2;
+                    newColor.b = (_percievedPlayerColor.b + objColor.b) / 2;
+
+                    _percievedPlayerColor = newColor;
+                }
+
+                else
+                {
+                    break;
+                }
+            }
         }
 
         else
-		{
-            _percievedPlayerColor = _player.GetComponentInChildren<Renderer>().material.color;
-			Debug.Log (_percievedPlayerColor);
+        {
+            _percievedPlayerColor = GetPlayerColor(_player);
         }
     }
 
@@ -154,64 +115,104 @@ public class EnemySight : MonoBehaviour
         return _percievedPlayerColor;
     }
 
-	public bool IsPlayerVisible()
-	{
-		return _playerVisible;
-	}
+    public bool IsPlayerVisible()
+    {
+        return _playerVisible;
+    }
 	
-	// Not currently used
-	float CalculatePathLength(Vector3 targetPosition)
-	{
-		NavMeshPath path = new NavMeshPath ();
-		if (_nav.enabled)
-			_nav.CalculatePath (targetPosition, path);
+    // Not currently used
+    float CalculatePathLength(Vector3 targetPosition)
+    {
+        NavMeshPath path = new NavMeshPath ();
+        if (_nav.enabled)
+            _nav.CalculatePath (targetPosition, path);
 
-		Vector3[] allWayPoints = new Vector3[path.corners.Length + 2];
+        Vector3[] allWayPoints = new Vector3[path.corners.Length + 2];
 
-		allWayPoints [0] = transform.position;
-		allWayPoints [allWayPoints.Length - 1] = targetPosition;
+        allWayPoints [0] = transform.position;
+        allWayPoints [allWayPoints.Length - 1] = targetPosition;
 
-		for (int i = 0; i < path.corners.Length; i++) {
-			allWayPoints [i + 1] = path.corners [i];
+        for (int i = 0; i < path.corners.Length; i++) {
+            allWayPoints [i + 1] = path.corners [i];
 
 		
-		}
-		float pathLength = 0f;
-		for (int i = 0; i < allWayPoints.Length - 1; i++) {
+        }
+        float pathLength = 0f;
+        for (int i = 0; i < allWayPoints.Length - 1; i++) {
 		
-			pathLength += Vector3.Distance (allWayPoints [i], allWayPoints [i + 1]);
+            pathLength += Vector3.Distance (allWayPoints [i], allWayPoints [i + 1]);
 
-		}
-		return pathLength;
-	}
+        }
+        return pathLength;
+    }
 
-	bool ChangesColor(string tag)
-	{
-		return tag.Equals("Red") ||
-		       tag.Equals("Orange") ||
-		       tag.Equals("Yellow") ||
-		       tag.Equals("Green") ||
-		       tag.Equals("Cyan") ||
-		       tag.Equals("Violet") ||
-		       tag.Equals("Pink") ||
-		       tag.Equals("White") ||
-		       tag.Equals("Player");
-	}
+    bool ChangesColor(string tag)
+    {
+        return tag.Equals("Red") ||
+               tag.Equals("Orange") ||
+               tag.Equals("Yellow") ||
+               tag.Equals("Green") ||
+               tag.Equals("Cyan") ||
+               tag.Equals("Violet") ||
+               tag.Equals("Pink") ||
+               tag.Equals("White") ||
+               tag.Equals("Player");
+    }
 
-	private Color GetPlayerColor(GameObject player)
-	{
-		foreach (var renderer in player.GetComponentsInChildren<Renderer>())
-		{
-			foreach (var mat in renderer.materials)
-			{
-				if (mat.name == "Player (Instance)")
-				{
-					return mat.color;
-				}
-			}
-		}
+    private Color GetPlayerColor(GameObject player)
+    {
+        foreach (var renderer in player.GetComponentsInChildren<Renderer>())
+        {
+            foreach (var mat in renderer.materials)
+            {
+                if (mat.name == "Player (Instance)")
+                {
+                    return mat.color;
+                }
+            }
+        }
 
-		Debug.Log("GET COLOR FAILURE");
-		return Color.white;
-	}
+        Debug.Log("GET COLOR FAILURE");
+        return Color.white;
+    }
+    
+    private Color GetColorValue(ColorName c)
+    {
+        Color colorValue;
+        
+        switch (c)
+        {
+            case ColorName.Red:
+                colorValue = Color.red;
+                break;
+            case ColorName.Orange:
+                colorValue = new Color(1, .5f, 0, 1);
+                break;
+            case ColorName.Yellow:
+                colorValue = Color.yellow;
+                break;
+            case ColorName.Green:
+                colorValue = Color.green;
+                break;
+            case ColorName.Cyan:
+                colorValue = Color.cyan;
+                break;
+            case ColorName.Violet:
+                colorValue = new Color(.5f, 0, 1, 1);
+                break;
+            case ColorName.Pink:
+                colorValue = new Color(1, .4f, .87f, 1);
+                break;
+            case ColorName.White:
+                colorValue = new Color(1, 1, 1, 1);
+                break;
+            case ColorName.Null:
+                colorValue = new Color(0, 0, 0, 0);
+                break;
+            default:
+                colorValue = Color.white;
+                break;
+        }
+        return colorValue;
+    }
 }
