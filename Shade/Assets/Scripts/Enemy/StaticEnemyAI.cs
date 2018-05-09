f70da23f6fd8126f7fd31440ba01a6883ab5353b
Justin <jseara2011@gmail.com>
2018-05-09 15:06:46 -0400

    Levels came, and then Enemy_Static: the final playtesting build.

--- Shade/Assets/Scripts/Enemy/StaticEnemyAI.cs
+++ Shade/Assets/Scripts/Enemy/StaticEnemyAI.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 
 public class StaticEnemyAI : MonoBehaviour
 {
-	private Spawn _spawnManager;
-	private EnemySight _sight;
-		
-	private void Start()
-	{
-		_spawnManager = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<Spawn>();
-		_sight = GetComponent<EnemySight>();
-	}
+    private Spawn _spawnManager;
+    private EnemySight _sight;
 
-	/* private void OnTriggerEnter(Collider other)
+    private void Start()
+    {
+        _spawnManager = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<Spawn>();
+        _sight = GetComponent<EnemySight>();
+    }
+
+    /* private void OnTriggerEnter(Collider other)
 	{
 		
 		Debug.Log("PlayerVisible: " + _sight.IsPlayerVisible());
@@ -25,16 +25,37 @@ public class StaticEnemyAI : MonoBehaviour
 		}
 	} */
 
-	private void OnTriggerStay(Collider other)
-	{
-		if (other.CompareTag("Player") && _sight.IsPlayerVisible() && _sight.GetPercievedColor() != GetEnemyColor())
-		{
-			_spawnManager.TriggerRespawn(other.gameObject);
-		}
-	}
+    private void OnTriggerStay(Collider other)
+    {
 
-	private Color GetEnemyColor()
-	{
-		return GetComponent<Renderer>().material.color;
-	}
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(IsEqualTo(_sight.GetPercievedColor(), GetEnemyColor()));
+            Debug.Log("Player Color: " + _sight.GetPercievedColor());
+            Debug.Log("Enemy Color: " + GetEnemyColor());
+        }
+        if (other.CompareTag("Player") && _sight.IsPlayerVisible() && !IsEqualTo(_sight.GetPercievedColor(), GetEnemyColor()))
+        {
+
+            _spawnManager.TriggerRespawn(other.gameObject);
+        }
+    }
+
+    private Color GetEnemyColor()
+    {
+        return transform.parent.GetComponentInChildren<Renderer>().material.color;
+    }
+
+    private static bool IsEqualTo(Color me, Color other)
+    {
+        bool isRedSimilar = false, isGreenSimilar = false, isBlueSimilar = false;
+        if (Mathf.Abs(other.r - me.r) < .1)
+            isRedSimilar = true;
+        if (Mathf.Abs(other.b - me.b) < .1)
+            isBlueSimilar = true;
+        if (Mathf.Abs(other.g - me.g) < .1)
+            isGreenSimilar = true;
+
+        return isRedSimilar && isBlueSimilar && isGreenSimilar;
+    }
 }
