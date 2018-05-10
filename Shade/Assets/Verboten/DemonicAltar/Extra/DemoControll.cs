99dc4360711a226c9c91b049d99a1a1b386b11ea
Justin <jseara2011@gmail.com>
2018-05-10 01:01:33 -0400

    donezo

--- Shade/Assets/Verboten/DemonicAltar/Extra/DemoControll.cs
+++ Shade/Assets/Verboten/DemonicAltar/Extra/DemoControll.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DemoControll : MonoBehaviour {
+public class DemoControll : MonoBehaviour
+{
 
     public Transform altarTF;
     public Light mainLight;
     public DemonicAltarController altarScript;
-
+    public Material demonSky;
+    public Material standardSky;
+    public GameObject privacyWall;
     private float lightMaxIntens, lightTargetIntens, timer;
     private bool altarStarted, fadeInLight, altarStopped;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
         lightMaxIntens = mainLight.intensity;
         //mainLight.intensity = 0;
         lightTargetIntens = lightMaxIntens;
+
     }
-	
-	// Update is called once per frame
-	void Update () 
+
+    // Update is called once per frame
+    void Update()
     {
         altarTF.Rotate(Vector3.up, 10 * Time.deltaTime);
 
@@ -29,10 +33,25 @@ public class DemoControll : MonoBehaviour {
 
     public void OnTriggerEnter()
     {
+        if (privacyWall.activeInHierarchy)
+        {
+            privacyWall.SetActive(false);
+        }
+        if (RenderSettings.skybox.name == "standardSky")
+        {
+            RenderSettings.skybox = demonSky;
+            DynamicGI.UpdateEnvironment();
+        }
+        else if (RenderSettings.skybox.name == "demonSky")
+        {
+            RenderSettings.skybox = standardSky;
+            DynamicGI.UpdateEnvironment();
+        }
+
         altarScript.ActivateDeactivateAltar();
         if (lightTargetIntens != 0)
-                 lightTargetIntens = 0;
-             else lightTargetIntens = lightMaxIntens;
+            lightTargetIntens = 0;
+        else lightTargetIntens = lightMaxIntens;
     }
     public void ButtonToggleAltar()
     {
